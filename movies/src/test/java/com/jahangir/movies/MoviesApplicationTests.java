e662f1d2bea81494e9a665b2e6baa19d869861ee
dr-scooby <hinurali@hotmail.com>
2024-04-05 18:19:45 -0700

    push files to movie git hub

---
+++ movies/src/test/java/com/jahangir/movies/MoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jahangir.movies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
